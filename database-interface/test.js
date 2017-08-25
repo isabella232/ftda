ce3558a273def3b8cf952144970aca092a95d893
Sean Tracey <seanmtracey@gmail.com>
2017-08-25 10:31:33 +0100

    Implement promise-based interface for SQL tables

---
+++ database-interface/test.js
@@ -0,0 +1,29 @@
+require('dotenv').config( { silent : false } );
+const debug = require('debug')('test');
+const database = require('./index');
+
+database.connect({
+		host : process.env.MYSQL_HOST,
+		user : process.env.MYSQL_USERNAME,
+		password : process.env.MYSQL_PASSWORD,
+		database : process.env.MYSQL_DATABASE,
+		port : process.env.MYSQL_PORT 
+	})
+	.then(function(){
+
+		database.query('SELECT * FROM slice')
+			.then(data => {
+				debug(data.results);
+				database.disconnect();
+			})
+			.catch(err => {
+				debug(err);
+				database.disconnect();
+			})
+		;
+		
+		console.log('what')
+		
+	})
+;
+
