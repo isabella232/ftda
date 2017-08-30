4de78ac889cd703efa8347e571aa4d2ec3b852de
Sean Tracey <seanmtracey@gmail.com>
2017-08-30 16:20:26 +0100

    Convert slicer to handle single pages with multiple articles

---
+++ slicer/index.js
@@ -0,0 +1,196 @@
+#!/usr/bin/env node
+
+const debug = require('debug')('slicer');
+const fs = require('fs');
+const co = require('co');
+const dotenv = require('dotenv').config();
+const uuid = require('uuid').v4;
+
+const imageProcessing = require('./lib/image-processing');
+
+const database = require('./lib/database-interface');
+
+const databaseConnectionDetails = {
+	host : process.env.SLICE_DATABASE_HOST,
+	user : process.env.SLICE_DATABASE_USERNAME,
+	password : process.env.SLICE_DATABASE_PASSWORD,
+	port : process.env.SLICE_DATABASE_PORT
+};
+
+const tmpPath = process.env.TMPPATH || '/tmp/';
+
+const AWS = require('aws-sdk');
+const S3 = new AWS.S3();
+
+let currentJob;
+let isProcessing = false;
+
+function getJobFromSliceQueue(){
+	// Get item from queue and return it
+	return database.query('SELECT * FROM slice ORDER BY rand() LIMIT 1')
+		.then(function(data){
+			return data.results[0];
+		})
+		.then(function(job){
+			return database.query(`UPDATE slice SET status = 'processing' WHERE id=${job.id}`)
+				.then(function(){
+					return job;
+				})
+		})
+	;
+}
+
+function deleteJobFromSliceQueue(id){
+	// Delete job from slice queue when it's been completed
+	return database.query(`DELETE * FROM slice WHERE id = ${id}`);
+}
+
+function resetJobInSliceQueue(job){
+	return database.query(`UPDATE slice SET status = 'available' WHERE id=${job.id}`);
+}
+
+function addJobToScanQueue(details){
+	// Add new item to scan queue
+	return database.query(`INSERT INTO scan (\`status\`, \`page-uuid\`, \`article-uuid\`, \`model-type\`) VALUES ("available", "${ details.parentID }", "${ details.articleID }", "default");`);
+}
+
+function processAndSlice(data){
+
+	isProcessing = true;
+	currentJob = data;
+
+	const random = uuid();
+
+	// Example data.id
+	// FTDA-1945-0108-0001
+	const number = data['page-uuid'].replace('FTDA-', '');
+	const noDash = number.replace(/-/g, '');
+
+	const resourcePath = noDash.slice(0, noDash.length - 4); // 19450108
+	const parentPageID = data['page-uuid'];
+	const articleSections = data.slices;
+
+	console.log("resourcePath:", resourcePath, "parentPageID:", parentPageID, "articleSections:", articleSections);
+
+	console.log(`${resourcePath}/${parentPageID}.JPG`);
+
+	const parentPageDestination = `${tmpPath}${random}.jpg`;
+	const file = fs.createWriteStream(parentPageDestination);
+	S3.getObject({
+		Bucket : 'artefacts.ftlabs-ftda.pages',
+		Key : `${resourcePath}/${parentPageID}.JPG`
+	}).createReadStream().pipe(file);
+
+	file.on('error', function(e){
+		console.log("error event");
+		console.log(e);
+	});
+
+	file.on('close', function(e){
+		console.log(`File recieved from S3 and written to ${parentPageDestination}`);
+
+		const articlesToProcess = articleSections.map(function(article){
+
+			/*
+				// Example article object
+				{"id":"FTDA-1945-0108-0001-001","coordinates":[["89","89","982","714"]]}
+			*/
+
+			return new Promise( function(resolve, reject){
+
+				imageProcessing.process(parentPageDestination, article.coordinates)
+					.then(img => {
+						console.log('img:', img);
+						console.log(data);
+		
+						fs.readFile(img.path, (err, file) => {
+							if(err){
+								console.log("Error reading spliced image:", err);
+							} else {
+								console.log("Spliced image read from disk:", img.path);
+		
+								new AWS.S3({
+									params : {
+										Bucket : 'artefacts.ftlabs-ftda.articles',
+										Key : `${article.id}.jpg`
+									}
+								}).upload({Body : file}, function(){
+									console.log(`Snippet ${article.id}.jpg successfully uploaded`);
+		
+									addJobToScanQueue( { parentID : parentPageID, articleID : article.id } )
+										.then(function(){
+											return deleteJobFromSliceQueue(data.id);
+										})
+										.then(function(){
+											resolve(true);
+										})
+										.catch(function(err){
+											debug('An error occurred adding a job to the queue', err);
+											reject(data);
+										})
+									;
+									
+								});
+		
+							}
+		
+						});
+		
+					})
+					.catch(err => {
+						console.log("ERR:", err);
+
+					})
+				;
+
+			});
+
+
+		});
+
+		Promise.all(articlesToProcess)
+			.then(function(){
+				isProcessing = false;
+			})
+			.catch(function(err){
+				debug('An error occurred proccessing one of the articles in the page', err);
+			})
+		;
+
+	});
+
+}
+
+database.connect(databaseConnectionDetails)
+	.then(function(){
+
+		setInterval(function(){
+		
+			if(!isProcessing){
+				getJobFromSliceQueue()
+					.then(function(job){
+
+						if(job !== undefined){
+							job.slices = JSON.parse(job.slices);
+							processAndSlice(job);
+						}
+
+					})
+				;
+			}
+		
+		}, 1000);
+
+	})
+	.catch(function(err){
+		debug('Top level error', err);
+		if(currentJob){
+			resetJobInSliceQueue(currentJob)
+				.then(function(){
+					currentJob = undefined;
+					isProcessing = false;
+				})
+			;
+		}
+	})
+;
